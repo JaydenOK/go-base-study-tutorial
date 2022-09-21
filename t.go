ea80ffc1b0e501e9a9277ec0defe8c4c138a3ea1
JaydenMan <603480498@qq.com>
2022-09-21 15:19:06 +0800

    json&map转换

--- t.go
+++ t.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 )
 
-/**
+/*
+*
 使用go module模式
 即使放在src目录下，src/project1,src/project2；每个目录下一个go.mod（命令:go mod init生成） 和 main.go文件
 src/project1/package1为package1包，其下文件的包名即为package package1(包名与目录保持一致)
 */
-func main() {
+func t() {
 	fmt.Println("hi")
 	package1.Import()
 }
