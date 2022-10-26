eff83f1613e50258714a2fa4280e9ca73d771cfc
JaydenOK <603480498@qq.com>
2022-10-26 18:27:37 +0800

    ''

---
+++ grpc2/client/client.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"base_project/grpc2/pb"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+//grpc 模拟客户端
+func main() {
+	// 1 创建客户端连接
+	conn, err := grpc.Dial("127.0.0.1:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println("grpc Dial err: ", err)
+		return
+	}
+	defer conn.Close()
+	// 2 创建远程服务的客户端
+	grpcClient := pb.NewOrderServiceClient(conn)
+
+	req := pb.OrderListRequest{OrderId: "aaa"}
+	// 3 发送 rpc 请求
+	res, err := grpcClient.GetList(context.Background(), &req)
+	if err != nil {
+		fmt.Println("grpcClient GetList err: ", err)
+		return
+	}
+	fmt.Println(res)
+}
---
+++ grpc2/server/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"base_project/grpc2/pb"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+)
+
+//grpc 服务端
+
+//实现 Service接口
+type OrderService struct {
+}
+
+//使用*pb.路径
+func (c *OrderService) GetList(ctx context.Context, req *pb.OrderListRequest) (*pb.OrderList, error) {
+	if req.OrderId == "" {
+		return nil, nil
+	}
+	//返回结果列表
+	return &pb.OrderList{
+		Id:           0,
+		OrderId:      req.OrderId,
+		ShipName:     "testShipName",
+		PlatformCode: "testPlatformCode",
+	}, nil
+}
+
+func main() {
+	// 1 初始化 grpc 对象
+	grpcServer := grpc.NewServer()
+	// 2 注册服务
+	pb.RegisterOrderServiceServer(grpcServer, new(OrderService))
+	// 3 创建监听
+	listener, err := net.Listen("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("net Listen err: ", err)
+		return
+	}
+	defer listener.Close()
+	// 4 绑定服务
+	grpcServer.Serve(listener)
+}
